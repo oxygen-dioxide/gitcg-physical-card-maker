a5b04dc2e536efcc519274bf1eeff1e15bc6ab66
unknown <1463567152@qq.com>
2026-08-18 06:42:03 +0800

    代码格式化

--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import "./style.css";
 import { init } from "./ui.js";
 
 async function loadFont() {
-  try {
-    const [hywh, hywh45w] = await Promise.all([
-      new FontFace('HYWH', 'url(assets/font/HYWH.ttf)').load(),
-      new FontFace('HYWH-55W', 'url(assets/font/HYWH-55W.ttf)').load().catch(() => null),
-    ])
-    document.fonts.add(hywh)
-    if (hywh45w) document.fonts.add(hywh45w)
-  } catch (_) {
-    console.warn('Font failed to load, using fallback')
-  }
+	try {
+		const [hywh, hywh45w] = await Promise.all([
+			new FontFace("HYWH", "url(assets/font/HYWH.ttf)").load(),
+			new FontFace("HYWH-55W", "url(assets/font/HYWH-55W.ttf)")
+				.load()
+				.catch(() => null),
+		]);
+		document.fonts.add(hywh);
+		if (hywh45w) document.fonts.add(hywh45w);
+	} catch (_) {
+		console.warn("Font failed to load, using fallback");
+	}
 }
 
-loadFont().then(init)
+loadFont().then(init);
